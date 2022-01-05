87bc938edf84bf0a566d1ecdb9e7c698dcc2d9ce
Triana Cerda <trianaRcerda@gmail.com>
2022-01-05 15:03:55 -0800

    added Register component + rendering to page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Login from './views/Login/Login';
 import Profile from './views/Profile/Profile';
+import Register from './views/Register/Register';
 
 export default function App() {
   return (
@@ -15,6 +16,9 @@ export default function App() {
         </Route>
         <Route path="/profile">
           <Profile />
+        </Route>
+        <Route path="/register">
+          <Register />
         </Route>
       </Switch>
     </Router>
---
+++ src/views/Register/Register.jsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>hello from register/sign up page</h1>
+    </div>
+  );
+}
