091960c0d6a250b1c090ffc7c64d3f63629d18be
Triana Cerda <trianaRcerda@gmail.com>
2022-01-05 11:05:24 -0800

    added Login component + rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
+import Login from './views/Login/Login';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route>
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
