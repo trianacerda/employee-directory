4be894e086cc1c2f0411e5102a46021f12b73300
Triana Cerda <trianaRcerda@gmail.com>
2022-01-05 11:11:32 -0800

    added Profile component + rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Login from './views/Login/Login';
+import Profile from './components/Profile/Profile';
 
 export default function App() {
   return (
@@ -11,6 +12,9 @@ export default function App() {
         </Route>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/profile">
+          <Profile />
         </Route>
       </Switch>
     </Router>
